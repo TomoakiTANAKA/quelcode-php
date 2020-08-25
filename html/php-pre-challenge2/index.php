e61da681e5dd2cd9f70ed84001af2cb7a1785022
TomoakiTANAKA <tanaka1987@gmail.com>
2020-08-25 19:15:57 +0900

    課題2の実装:バブルソート

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,6 +3,21 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
+    
+    if($i == 0) continue;
+
+    // 現在のループindexの数値を左隣と比較して、る比べて大きい内は繰り返す
+    $j = $i;
+    while($j > 0) {
+        if ($array[$j] < $array[$j-1]) {
+            // swap
+            $temp = $array[$j];
+            $array[$j] = $array[$j-1];
+            $array[$j-1] = $temp;
+        }
+
+        $j--;
+    }
 
 }
 // 修正はここまで
@@ -10,3 +25,6 @@ for ($i = 0; $i < count($array); $i++) {
 echo "<pre>";
 print_r($array);
 echo "</pre>";
+
+// 小さい順にならべる
+// http://localhost:10080/php-pre-challenge2/index.php?array=3,2,1,4,15,18,13,99,77,66,1,100,0
